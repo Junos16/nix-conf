1bd0e3ac96219e19d11c312d56908c0e8f918b4c
Junos16 <hriddhitdatta2002@gmail.com>
2026-09-23 22:29:53 +0530

    Add home manager config

--- flake.nix
+++ flake.nix
@@ -3,14 +3,27 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
+
+    home-manager = {
+      url = "github:nix-community/home-manager/release-26.05";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs }: {
+  outputs = { self, nixpkgs, home-manager }: {
     nixosConfigurations.iitk-lab = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
 
       modules = [
         ./hosts/iitk-lab/configuration.nix
+	
+	home-manager.nixosModules.home-manager
+
+	{
+	  home-manager.useGlobalPkgs = true;
+	  home-manager.users.hriddhit = 
+	    import ./home/hriddhit/home.nix;
+	}
       ];
     };
   };
---
+++ home/hriddhit/home.nix
@@ -0,0 +1,8 @@
+{ ... }:
+
+{
+  home.username = "hriddhit";
+  home.homeDirectory = "/home/hriddhit";
+
+  home.stateVersion = "26.05";
+}
