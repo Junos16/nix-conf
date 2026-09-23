555fbf8d64c965d6671c1e7005c3061481f85923
Junos16 <hriddhitdatta2002@gmail.com>
2026-09-23 22:53:07 +0530

    Add pre-wm packages

--- home/hriddhit/home.nix
+++ home/hriddhit/home.nix
@@ -1,8 +1,16 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
   home.username = "hriddhit";
   home.homeDirectory = "/home/hriddhit";
+
+  home.packages = with pkgs; [
+    tmux
+    fastfetch
+    fzf
+    ripgrep
+    jq
+  ];
 
   home.stateVersion = "26.05";
 }
--- hosts/iitk-lab/configuration.nix
+++ hosts/iitk-lab/configuration.nix
@@ -70,7 +70,6 @@
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     wget
     git
-    tmux
     curl
     python3
   ];
