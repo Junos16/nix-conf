6f595132088e297d2bba62dae704a1714be6cc8b
Junos16 <hriddhitdatta2002@gmail.com>
2026-09-24 23:46:05 +0530

    Change config for sway to nixlang

--- home/hriddhit/home.nix
+++ home/hriddhit/home.nix
@@ -1,4 +1,4 @@
-{ pkgs, codexPackage, ... }:
+{ pkgs, codexPackage, lib, ... }:
 
 {
   home.username = "hriddhit";
@@ -19,6 +19,7 @@
     spotify
     discord
     zoom
+    stremio-linux-shell
 
     wl-clipboard
     grim
@@ -115,66 +116,75 @@
     '';
   };
 
-  xdg.configFile."sway/config".text = ''
-    include /etc/sway/config.d/*
-    
-    set $mod Mod4
-    set $term foot
-    set $menu fuzzel
-    
-    # Terminal
-    bindsym $mod+Return exec $term
+  wayland.windowManager.sway = {
+    enable = true;
+    package = null;
 
-    # Menu
-    bindsym $mod+d exec $menu
+    systemd.enable = true;
 
-    # Screenshots
-    bindsym Print exec grim -g "$(slurp)" - | wl-copy
-    bindsym $mod+Shift+s  exec grim -g "$(slurp)" - | wl-copy
-    bindsym Shift+Print exec grim - | wl-copy
-    bindsym $mod+s exec grim - | wl-copy
+    config = {
+      modifier = "Mod4";
+      terminal = "foot";
+      menu = "fuzzel";
 
-    # Lockscreen 
-    bindsym $mod+Ctrl+l exec swaylock -f
+      keybindings = lib.mkForce {
+        "Mod4+Return" = "exec foot";
+        "Mod4+d" = "exec fuzzel";
 
-    #Close focused window
-    bindsym $mod+Shift+q kill
+        "Print" = ''exec grim -g "$(slurp)" - | wl-copy'';
+        "Mod4+Shift+s" = ''exec grim -g "$(slurp)" - | wl-copy'';
+        "Shift+Print" = "exec grim - | wl-copy";
+        "Mod4+s" = "exec grim - | wl-copy";
 
-    # Focus
-    bindsym $mod+h focus left
-    bindsym $mod+j focus down
-    bindsym $mod+k focus up
-    bindsym $mod+l focus right
+        "Mod4+Ctrl+l" = "exec swaylock -f";
+        "Mod4++q" = "kill";
 
-    # Move windows
-    bindsym $mod+Shift+h move left
-    bindsym $mod+Shift+j move down
-    bindsym $mod+Shift+k move up
-    bindsym $mod+Shift+l move right
+        "Mod4+h" = "focus left";
+        "Mod4+j" = "focus down";
+        "Mod4+k" = "focus up";
+        "Mod4+l" = "focus right";
 
-    # Workspaces
-    bindsym $mod+1 workspace number 1
-    bindsym $mod+2 workspace number 2
-    bindsym $mod+3 workspace number 3
-    bindsym $mod+4 workspace number 4
-    bindsym $mod+5 workspace number 5
+        "Mod4+Shift+h" = "move left";
+        "Mod4+Shift+j" = "move down";
+        "Mod4+Shift+k" = "move up";
+        "Mod4+Shift+l" = "move right";
 
-    bindsym $mod+Shift+1 move container to workspace number 1
-    bindsym $mod+Shift+2 move container to workspace number 2
-    bindsym $mod+Shift+3 move container to workspace number 3
-    bindsym $mod+Shift+4 move container to workspace number 4
-    bindsym $mod+Shift+5 move container to workspace number 5
+        "Mod4+1" = "workspace number 1";
+        "Mod4+2" = "workspace number 2";
+        "Mod4+3" = "workspace number 3";
+        "Mod4+4" = "workspace number 4";
+        "Mod4+5" = "workspace number 5";
 
-    gaps inner 6
-    gaps outer 3
-    default_border pixel 2
+        "Mod4+Shift+1" = "move container to workspace number 1";
+        "Mod4+Shift+2" = "move container to workspace number 2";
+        "Mod4+Shift+3" = "move container to workspace number 3";
+        "Mod4+Shift+4" = "move container to workspace number 4";
+        "Mod4+Shift+5" = "move container to workspace number 5";
 
-    bindsym $mod+Shift+c reload
-    bindsym $mod+Shift+e exit
-    
-    exec waybar
-    output * bg #11111b solid_color
-  '';
+        "Mod4+Shift+c" = "reload";
+        "Mod4+Shift+e" = "exit";
+      };
+
+      modes = { };
+      bars = [ ];
+
+      gaps = {
+        inner = 6;
+        outer = 3;
+      };
+
+      window = {
+        titlebar = false;
+        border = 2;
+      };
+
+      startup = [
+        { command = "waybar"; }
+      ];
+
+      output."*".bg = "#11111b solid_color";
+    };
+  };
 
   services.mako.enable = true;
   services.polkit-gnome.enable = true;
